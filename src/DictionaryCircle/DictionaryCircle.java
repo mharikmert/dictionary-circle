3a2318765acf049a705f5d9647c368a9d7145cf8
mhilmiarikmert <mhilmiarikmert@gmail.com>
2020-08-01 01:41:58 +0300

    Structure and finding the word algorithm is changed, possible words founded

--- src/DictionaryCircle/DictionaryCircle.java
+++ src/DictionaryCircle/DictionaryCircle.java
@@ -1,5 +1,4 @@
 package DictionaryCircle;
-import java.util.ArrayList;
 public class DictionaryCircle {
     //Node class of the linked list
     public class Node {
@@ -8,15 +7,6 @@ public class DictionaryCircle {
         Node previous;
 
         public Node(int data) {
-            this.data = data;
-        }
-    }
-    public class newNode {
-        int data;
-        newNode newNode;
-        newNode previousNode;
-
-        public newNode(int data) {
             this.data = data;
         }
     }
@@ -42,7 +32,7 @@ public class DictionaryCircle {
             tail.next = head;
         }
     }
-    public static boolean clockWise() {
+    public static boolean randomBoolean() {
         return Math.random() < 0.5;
     }
     //void print method to see what is in the linked list
@@ -52,92 +42,55 @@ public class DictionaryCircle {
             System.out.print(" " + current.data);
             current = current.next;
         } while (current != head);
+        System.out.println("\n");
     }
     public static void main(String[] args) {
-        DictionaryCircle cl = new DictionaryCircle();
-        //Adds data to the list clockwise
-        //int clockwise = (int)(Math.random() * 2 + 1);
-        if (clockWise()) {// True or false, the loop should be toward clock wise or counter-clock wise
-            cl.add(1);cl.add(9);cl.add(6);cl.add(1);cl.add(7);cl.add(3);cl.add(6);cl.add(4);cl.add(8);cl.add(0);
-        } else {//toward to the other side
-            cl.add(1);cl.add(0);cl.add(8);cl.add(4);cl.add(6);cl.add(3);cl.add(7);cl.add(1);cl.add(6);cl.add(9);
-        }
-
-        /****
-         * Linked list part over, now creating the word.
-         ****/
-        String first[] = {"sıfır", "bir", "iki", "üç", "dört", "beş", "altı", "yedi", "sekiz", "dokuz"};
-        String second[] = {"", "on", "yirmi", "otuz", "kırk", "elli", "altmış", "yetmiş", "seksen", "doksan"};
-
-        String str = null, newStr = "";
-        char ch = '\0', currentCh = '\0', nextCh = '\0';
-        Node current = head;
-        // for the start point of the list
-        int orderTop = (int)(Math.random() * 9 + 1);
-        int startPoint = 0;
-        while(startPoint < orderTop){
-            System.out.println("order Top : " + orderTop + "current: "+current.data);
-            head = current;
-            current = current.next;
-            startPoint++;
-        }
-        System.out.println("head: "+ head.data);
-        print(head);
-        /*
-        do{
-            if(current.data == orderTop){
-                //make current node head of list.
+        char [] vowels = {'a','e','ı','i','o','ö','ü','u'};
+        for(int i = 0; i< 1000; i++){
+            String str = "", newStr = "";
+            DictionaryCircle cl = new DictionaryCircle();
+            //Adds data to the list clockwise
+            //int clockwise = (int)(Math.random() * 2 + 1);
+            if (randomBoolean()) {// True or false, the loop should be toward clock wise or counter-clock wise
+                cl.add(1);cl.add(9);cl.add(6);cl.add(1);cl.add(7);cl.add(3);cl.add(6);cl.add(4);cl.add(8);cl.add(0);
+            } else {//toward to the other side
+                cl.add(1);cl.add(0);cl.add(8);cl.add(4);cl.add(6);cl.add(3);cl.add(7);cl.add(1);cl.add(6);cl.add(9);
+            }
+            Node current = head;
+            int orderTop = (int)(Math.random() * 9 + 1);
+            int startPoint = 0;
+            while(startPoint < orderTop){
                 head = current;
-            }
-            current = current.next;
-        }while(current != head);
-
-        //System.out.println("head: " + head.data);
-        int var = 0, digitOneCounter = 0, digitTwoCounter = 0;
-
-        //to create 6 numbers from 10 numbers
-        while(var < 6){
-            int digit = (int)(Math.random()* 2 + 1);
-            //for  not being one digit number more than two
-            if(digit == 1 && digitOneCounter == 2){
-                //if variable definer passes 2, then digit would be two digit now and no more one digit number.
-                digit = 2;
-            }
-            //for not being five two digit numbers
-            if(digit == 2 && digitTwoCounter == 4){
-                digit = 1;
-            }
-            //One digit condition
-            if(digit == 1){
-                str = first[current.data];
-                //System.out.println("current data: " + current.data + " corrent next: " + current.next.data);
                 current = current.next;
-                digitOneCounter++;
+//                System.out.println("order Top : " + orderTop + "current: "+current.data);
+                startPoint++;
             }
-            //Two digit contdition
-            else if(digit == 2){
-                int newInt = current.data * 10 + current.next.data;
-                //condition for writing of two digit number that contains from a number and zero.- 10 multiple numbers
-                if(current.next.data == 0){
-                    str = second[newInt/10];
+            int [] numbers = new int [6];
+            int numberCounter = 0, oneDigitCounter = 0;
+            while(numberCounter < 6){
+                if(randomBoolean() && oneDigitCounter < 2){//one digit situation
+                    numbers[numberCounter] = current.data;
+                    oneDigitCounter++;
+                }else { //two digit situation
+                    numbers[numberCounter] = current.data *10 + current.next.data;
+                    current = current.next;
                 }
-                //normal number writing rule.
-                else str = second[newInt / 10] + first[newInt % 10];
-
-                //due to the number has two digits, next number in the linked list should be two digits after.
-                current = current.next.next;
-                digitTwoCounter++;
+                current = current.next;
+                numberCounter++;
             }
-            // Randomly picking first or last character of the numbers' writing
-            int pick = (int)(Math.random()* 2 + 1);
-            if(pick == 1)
-                ch = str.charAt(0);
-            else if(pick == 2)
-                ch = str.charAt(str.length() - 1);
-            newStr += "" + ch;
-            var++;
+            for(int j = 0; j < numbers.length; j++){
+                String units[] = {"", "bir", "iki", "üç", "dört", "beş", "altı", "yedi", "sekiz", "dokuz"};
+                String tens[] = {"", "on", "yirmi", "otuz", "kırk", "elli", "altmış", "yetmiş", "seksen", "doksan"};
+                if(numbers[j] >= 10){
+                    str = tens[numbers[j] /10] + units[numbers[j] % 10];
+                  newStr += randomBoolean() ? str.charAt(0) : str.charAt(str.length()-1);
+                }else{
+                    str = numbers[j] == 0 ? "sıfır" : units[numbers[j]];
+                    newStr += randomBoolean() ? str.charAt(0) : str.charAt(str.length()-1);
+                }
+            }
+            System.out.println("word: " + newStr);
+            // Write a turkish word validation method
         }
-        System.out.println("the word is: " + newStr);
-    }     */
     }
 }
