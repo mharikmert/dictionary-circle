25dd6679832146b571ba60d5b58684ba28fc16c4
mhilmiarikmert <mhilmiarikmert@gmail.com>
2020-07-31 20:40:19 +0300

    edition for determinating the head node

--- src/DictionaryCircle/DictionaryCircle.java
+++ src/DictionaryCircle/DictionaryCircle.java
@@ -1,22 +1,22 @@
 package DictionaryCircle;
 import java.util.ArrayList;
 public class DictionaryCircle {
-
     //Node class of the linked list
-    public class Node{
+    public class Node {
         int data;
         Node next;
         Node previous;
+
         public Node(int data) {
             this.data = data;
         }
     }
-    public class newNode{
+    public class newNode {
         int data;
         newNode newNode;
         newNode previousNode;
 
-        public newNode(int data){
+        public newNode(int data) {
             this.data = data;
         }
     }
@@ -25,19 +25,16 @@ public class DictionaryCircle {
     public static Node tail = null;
 
     //This function adds the new node at the end of the list.
-    public void add(int data){
+    public void add(int data) {
         //Create new node
         Node newNode = new Node(data);
         //Checks if the list is empty.
-        if(head == null) {
+        if (head == null) {
             //If list is empty, head and tail should point to new node.
             head = newNode;
             tail = newNode;
             newNode.next = tail;
-        }
-
-        else {
-            //tail will point to new node.
+        } else {
             tail.next = newNode;
             //New node will become new tail.
             tail = newNode;
@@ -45,40 +42,48 @@ public class DictionaryCircle {
             tail.next = head;
         }
     }
-    public static boolean clockWise(){return Math.random() < 0.5;}
+    public static boolean clockWise() {
+        return Math.random() < 0.5;
+    }
     //void print method to see what is in the linked list
-    public void print(){
+    public static void print(Node head) {
         Node current = head;
-        do{
+        do {
             System.out.print(" " + current.data);
             current = current.next;
-        }while(current != head);
+        } while (current != head);
     }
     public static void main(String[] args) {
         DictionaryCircle cl = new DictionaryCircle();
         //Adds data to the list clockwise
         //int clockwise = (int)(Math.random() * 2 + 1);
-        if(clockWise()){// True or false, the loop should be toward clock wise or counter-clock wise
-            //Addding argumnets that we want to the linked list.
-            cl.add(1); cl.add(9); cl.add(6); cl.add(1); cl.add(7); cl.add(3); cl.add(6); cl.add(4); cl.add(8); cl.add(0);
-        }
-        else{//toward to the other side
-            cl.add(1); cl.add(0); cl.add(8); cl.add(4); cl.add(6); cl.add(3); cl.add(7); cl.add(1); cl.add(6); cl.add(9);
+        if (clockWise()) {// True or false, the loop should be toward clock wise or counter-clock wise
+            cl.add(1);cl.add(9);cl.add(6);cl.add(1);cl.add(7);cl.add(3);cl.add(6);cl.add(4);cl.add(8);cl.add(0);
+        } else {//toward to the other side
+            cl.add(1);cl.add(0);cl.add(8);cl.add(4);cl.add(6);cl.add(3);cl.add(7);cl.add(1);cl.add(6);cl.add(9);
         }
 
         /****
          * Linked list part over, now creating the word.
          ****/
-        String first[] = {"sıfır","bir","iki","üç","dört","beş","altı","yedi","sekiz","dokuz"};
-        String second[] = {"","on","yirmi","otuz","kırk","elli","altmış","yetmiş","seksen","doksan"};
+        String first[] = {"sıfır", "bir", "iki", "üç", "dört", "beş", "altı", "yedi", "sekiz", "dokuz"};
+        String second[] = {"", "on", "yirmi", "otuz", "kırk", "elli", "altmış", "yetmiş", "seksen", "doksan"};
 
         String str = null, newStr = "";
-        char ch = '\0',currentCh = '\0', nextCh = '\0';
-        ArrayList<String> wordList = new ArrayList<String>();
-
+        char ch = '\0', currentCh = '\0', nextCh = '\0';
         Node current = head;
         // for the start point of the list
         int orderTop = (int)(Math.random() * 9 + 1);
+        int startPoint = 0;
+        while(startPoint < orderTop){
+            System.out.println("order Top : " + orderTop + "current: "+current.data);
+            head = current;
+            current = current.next;
+            startPoint++;
+        }
+        System.out.println("head: "+ head.data);
+        print(head);
+        /*
         do{
             if(current.data == orderTop){
                 //make current node head of list.
@@ -121,34 +126,18 @@ public class DictionaryCircle {
 
                 //due to the number has two digits, next number in the linked list should be two digits after.
                 current = current.next.next;
-                //   current.next.data =current.next.next.data;
                 digitTwoCounter++;
             }
-            //System.out.println(str);
-
-            // Randomly picking first or last charachter of the numbers' writing
+            // Randomly picking first or last character of the numbers' writing
             int pick = (int)(Math.random()* 2 + 1);
             if(pick == 1)
                 ch = str.charAt(0);
             else if(pick == 2)
                 ch = str.charAt(str.length() - 1);
-
-           /* for(int i = 0; i < str.length(); i++){
-                currentCh = str.charAt(i); nextCh = str.charAt(i + 1);
-                if(currentCh == 'a'){
-                    switch(nextCh){
-                        case 'o': break;
-                    }
-                }
-            }*/
-
-            //adding first or last charachters randomly to new String to create a new word.
             newStr += "" + ch;
-            //System.out.println("number: " + str);
             var++;
         }
         System.out.println("the word is: " + newStr);
+    }     */
     }
 }
-
-
