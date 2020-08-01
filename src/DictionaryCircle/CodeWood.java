8bd855bbda54f49671b27da8a9e0de185e4f3f4a
mhilmiarikmert <mhilmiarikmert@gmail.com>
2020-08-01 23:53:20 +0300

    codeWood is clear

--- src/DictionaryCircle/CodeWood.java
+++ src/DictionaryCircle/CodeWood.java
@@ -1,21 +1,5 @@
 package DictionaryCircle;
 public class CodeWood {
-    public static void main(String []args){
-        int [] numerals = {1,9,6,1,7,3,6,4,8,0};
-        int reverseNumerals [] = reverse(numerals);
-        for(int i= 0; i < reverseNumerals.length-1; i++){
-            System.out.println(reverseNumerals[i]);
-        }
-    }
-    public static int [] reverse(int [] array ){
-        int [] reversedArray = new int [array.length + 1];
-        for(int i = 0; i < array.length; i++){
-            reversedArray[i] = array[array.length-i-1];
-        }
-        reversedArray[array.length] = array[array.length-1];
-        return reversedArray;
-    }
-    public static boolean chance(){
-        return Math.random() < 0.5;
+    public static void main(String []args) {
     }
 }
