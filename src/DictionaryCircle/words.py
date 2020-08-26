4d543e97ce618300e50a2f54a4a8156a480d9326
mhilmiarikmert <mhilmiarikmert@gmail.com>
2020-08-26 15:30:15 +0300

    reading the whole words and writing the turkish ones

--- src/DictionaryCircle/words.py
+++ src/DictionaryCircle/words.py
@@ -3,4 +3,13 @@ from turkishnlp import detector
 obj = detector.TurkishNLP()
 obj.download()
 obj.create_word_set()
-print(obj.is_turkish("merhaba"))
+
+file1 = open('words.txt', 'r')
+Lines = file1.readlines()
+count = 0
+for line in Lines:
+    if obj.is_turkish(line):
+        file2 = open('turkishWords.txt','a')
+        file2.write(line)
+        count+= 1
+        # print("Line{}: {}".format(count, line.strip()))
